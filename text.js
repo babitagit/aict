2e3fef0c3cc5fa8870107952188efeafef40078c
skr <kumarsugam2609@gmail.com>
2023-07-02 23:57:59 +0530

    i m skr

---
+++ text.js
@@ -0,0 +1,7 @@
+// for pushing code to github 
+// 1=>  git add .
+//2=> git commit -m "enter your msg here"
+//3=> git push origin main/master
+
+// code ko update krne ke liye 
+//==> git pull origin main/master
